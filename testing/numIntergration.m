a0916b72a14a5dab1c2ab5ac9f99e34b934e8e70
Evangelos Vandoros <vandvag@pop-os.localdomain>
2019-12-08 12:05:55 +0100

    Generalized integrand equations

--- testing/numIntergration.m
+++ testing/numIntergration.m
@@ -10,8 +10,8 @@ phi11 = @(x) x / l;
 phi12 = @(x) 2 - x/l;
 phi21 = @(x) 0;
 phi22 = @(x) x / l - 1;
-matInt1 = @(x) 1/(pi*l) * (log((tan(tau*(1*l + x)) * tan(tau*abs((1-1)*l-x))) / (tan(tau*abs(1*l - x)) * tan(tau*((1-1)*l+x)))) - log((tan(tau*((1+1)*l + x)) * tan(tau*abs((1)*l-x))) / (tan(tau*abs((1+1)*l - x)) * tan(tau*((1)*l+x)))));
-matInt2 = @(x) 1/(pi*l) * log((tan(tau*(2*l + x)) * tan(tau*abs((2-1)*l-x))) / (tan(tau*abs(2*l - x)) * tan(tau*((2-1)*l+x))));
+matInt1 = @(x, n) 1/(pi*l) * (log((tan(tau*(n*l + x)) * tan(tau*abs((n-1)*l-x))) / (tan(tau*abs(n*l - x)) * tan(tau*((n-1)*l+x)))) - log((tan(tau*((n+1)*l + x)) * tan(tau*abs((n)*l-x))) / (tan(tau*abs((n+1)*l - x)) * tan(tau*((n)*l+x)))));
+matInt2 = @(x, n) 1/(pi*l) * log((tan(tau*(n*l + x)) * tan(tau*abs((n-1)*l-x))) / (tan(tau*abs(n*l - x)) * tan(tau*((n-1)*l+x))));
 
 sumArr = zeros(20,1);
 
@@ -27,19 +27,19 @@ sum21 = 0;
 sum22 = 0;
 
 for i=1:length(xj1)
-   sum11 = sum11 + wj1(i) * phi11(l/2*(xj1(i)+1)) * matInt1(l/2*(xj1(i)+1)) * (1-xj1(i))^(-0.5);
-   sum12 = sum12 + wj1(i) * phi11(l/2*(xj1(i)+1)) * matInt2(l/2*(xj1(i)+1)) * (1-xj1(i))^(-0.5);
-   sum21 = sum21 + wj1(i) * phi21(l/2*(xj1(i)+1)) * matInt1(l/2*(xj1(i)+1)) * (1-xj1(i))^(-0.5);
-   sum22 = sum22 + wj1(i) * phi21(l/2*(xj1(i)+1)) * matInt2(l/2*(xj1(i)+1)) * (1-xj1(i))^(-0.5);
+   sum11 = sum11 + wj1(i) * phi11(l/2*(xj1(i)+1)) * matInt1(l/2*(xj1(i)+1), 1) * (1-xj1(i))^(-0.5);
+   sum12 = sum12 + wj1(i) * phi11(l/2*(xj1(i)+1)) * matInt2(l/2*(xj1(i)+1), 2) * (1-xj1(i))^(-0.5);
+   sum21 = sum21 + wj1(i) * phi21(l/2*(xj1(i)+1)) * matInt1(l/2*(xj1(i)+1), 1) * (1-xj1(i))^(-0.5);
+   sum22 = sum22 + wj1(i) * phi21(l/2*(xj1(i)+1)) * matInt2(l/2*(xj1(i)+1), 2) * (1-xj1(i))^(-0.5);
 end
 
 % Integration from l to 2l
 
 for i=1:length(xj1)
-    sum11 = sum11 + wj2(i) * phi12(l/2*(xj2(i)+3)) * matInt1(l/2*(xj2(i)+3)) * (1+xj2(i))^(-0.5);
-    sum12 = sum12 + wj2(i) * phi12(l/2*(xj2(i)+3)) * matInt2(l/2*(xj2(i)+3)) * (1+xj2(i))^(-0.5);
-    sum21 = sum21 + wj1(i) * phi22(l/2*(xj1(i)+3)) * matInt1(l/2*(xj1(i)+3)) * (1-xj1(i))^(-0.5);
-    sum22 = sum22 + wj1(i) * phi22(l/2*(xj1(i)+3)) * matInt2(l/2*(xj1(i)+3)) * (1-xj1(i))^(-0.5);
+    sum11 = sum11 + wj2(i) * phi12(l/2*(xj2(i)+3)) * matInt1(l/2*(xj2(i)+3), 1) * (1+xj2(i))^(-0.5);
+    sum12 = sum12 + wj2(i) * phi12(l/2*(xj2(i)+3)) * matInt2(l/2*(xj2(i)+3), 2) * (1+xj2(i))^(-0.5);
+    sum21 = sum21 + wj1(i) * phi22(l/2*(xj1(i)+3)) * matInt1(l/2*(xj1(i)+3), 1) * (1-xj1(i))^(-0.5);
+    sum22 = sum22 + wj1(i) * phi22(l/2*(xj1(i)+3)) * matInt2(l/2*(xj1(i)+3), 2) * (1-xj1(i))^(-0.5);
 end
 
 sum11 = sum11 * (1 / (2*pi*l));
