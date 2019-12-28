1fad822e51e5e26348eb7099e4a3865c16b49674
Evangelos Vandoros <vandvag1994@gmail.com>
2019-12-28 19:35:50 +0100

    Added files to rewrite

---
+++ testing/numIntNew.m
@@ -0,0 +1,10 @@
+clear all
+
+c  = 1;
+p0 = 1;
+n  = 2;
+a  = 2;
+l  = a / n;
+tau = pi / (4*a);
+
+% System matrix M
---
+++ testing/tester.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matlplotlib.pyplot as plt
+
+a = 3
+print(a)
